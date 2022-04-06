1e6cafbc8354d21006f380242b078fa57dc6fd2f
Piter <piterwongso6@gmail.com>
2022-04-06 22:53:01 +0700

    Add certain components to HomeView

---
+++ masakind/View Controller/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  masakind
+//
+//  Created by Piter Wongso on 06/04/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
